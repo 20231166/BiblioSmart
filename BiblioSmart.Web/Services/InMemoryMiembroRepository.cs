710e679d655a6612a0a36fea51e43aca2801fa51
sheyssi04 <sheyssipa@gmail.com>
2026-04-17 13:09:13 -0400

    Personaliza BiblioSmart con mis datos y nueva identidad visual

--- BiblioSmart.Web/Services/InMemoryMiembroRepository.cs
+++ BiblioSmart.Web/Services/InMemoryMiembroRepository.cs
@@ -7,8 +7,8 @@ namespace BiblioSmart.Web.Services
     {
         private readonly List<Miembro> _miembros = new()
         {
-            new Miembro { Id = 1, NombreCompleto = "Romaldi Joel Mosquea", Cedula = "001-2023191-0", Correo = "romaldi@mail.com", Telefono = "809-555-0001", FechaRegistro = DateTime.Now },
-            new Miembro { Id = 2, NombreCompleto = "María García", Cedula = "002-1234567-8", Correo = "maria@mail.com", Telefono = "809-555-0002", FechaRegistro = DateTime.Now },
+            new Miembro { Id = 1, NombreCompleto = "Sheyssi Puente Adames", Cedula = "001-0000000-1", Correo = "sheyssi@bibliosmart.local", Telefono = "809-555-0001", FechaRegistro = DateTime.Now },
+            new Miembro { Id = 2, NombreCompleto = "Maria Garcia", Cedula = "002-1234567-8", Correo = "maria@bibliosmart.local", Telefono = "809-555-0002", FechaRegistro = DateTime.Now },
         };
         private int _nextId = 3;
 
@@ -16,7 +16,8 @@ namespace BiblioSmart.Web.Services
         public Task<Miembro?> GetByIdAsync(int id) => Task.FromResult(_miembros.FirstOrDefault(m => m.Id == id));
         public Task<Miembro?> GetByCedulaAsync(string cedula) => Task.FromResult(_miembros.FirstOrDefault(m => m.Cedula == cedula));
         public Task AddAsync(Miembro miembro) { miembro.Id = _nextId++; _miembros.Add(miembro); return Task.CompletedTask; }
-        public Task UpdateAsync(Miembro miembro) {
+        public Task UpdateAsync(Miembro miembro)
+        {
             var i = _miembros.FindIndex(m => m.Id == miembro.Id);
             if (i >= 0) _miembros[i] = miembro;
             return Task.CompletedTask;
